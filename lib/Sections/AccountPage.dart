852b66cc41bef3ef2ee673ede7ff083dfcbed528
myorh <mayowaolunuga1@gmail.com>
2019-01-07 23:52:52 +0100

    view todo items in todo list

--- lib/Sections/AccountPage.dart
+++ lib/Sections/AccountPage.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CompletedPage extends StatelessWidget {
+class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
-      title: Text('Completed'),
+      title: Text('Account'),
     ),
       body: Center(
-          child: Text('Hello Completed ')
+          child: Text('Hello Account ')
       ),
     );
   }
--- lib/Sections/TodayPage.dart
+++ lib/Sections/TodayPage.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/Widgets//CustomCheckbox.dart';
+import 'package:flutter_todo/Widgets/TodoItem.dart';
+
 
 class TodayPage extends StatelessWidget {
 
+
+  void itemClicked(index){
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,74 +18,12 @@ class TodayPage extends StatelessWidget {
 
       body: Center(
           child: ListView.builder(
+            itemCount: 10,
               itemBuilder: (BuildContext context, int index){
-             for (var i = 0; i < 10; i ++){
               return TodoItem(index);
-            }
+
           })
       ),
     );
   }
 }
-
-
-
-class TodoItem extends StatefulWidget{
-  TodoItem(this.index);
-  final int index;
-
-  @override
-  State<StatefulWidget> createState()  => TodoItemState();
-
-}
-
-class TodoItemState extends State<TodoItem>{
-  bool _checked = false;
-
-  void _onCheckBoxChange(){
-    setState(() {
-      _checked = !_checked;
-    });
-  }
-
-  void _voidCall(isChecked) => {};
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    GestureDetector detector  = GestureDetector(
-      child: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(right: 15),
-              child:CustomCheckbox(
-                  value: _checked,
-                  materialTapTargetSize: null,
-                  onChanged: _voidCall,
-                  useTapTarget: false,
-                  activeColor: Colors.teal),
-            ),
-
-            Expanded(
-                child:  Text('This is supposed to be a long text that should '
-                    'wrap around using softwrap',
-                  softWrap: true,)
-            )
-
-          ],
-        ),
-        padding: EdgeInsets.all(16.0),
-      ),
-      onTap: _onCheckBoxChange,
-    );
-
-    // TODO: implement build
-    return detector;
-  }
-}
-
