422ade11d8b1139df49b860e0367e3c5748135f6
myorh <backupbox86@gmail.com>
2019-01-23 01:07:17 +0100

    Fix expanded.

--- lib/Widgets/TodoItem.dart
+++ lib/Widgets/TodoItem.dart
@@ -34,20 +34,50 @@ class TodoItem extends StatelessWidget {
                   useTapTarget: false,
                   activeColor: Colors.teal),
             ),
-            Column(
-              children: <Widget>[
-                Text(text),
-                Container(
-                    child: FlatButton(
-                        child: Text("Edit"),
-                      onPressed: null,
-                    )
-                )
-              ],
-            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    text,
+                    style: TextStyle(
+                        color: Colors.black87, fontWeight: FontWeight.w500),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Chip(
+                        label: Text("Work"),
+                        labelStyle: TextStyle(fontSize: 9, color: Colors.black),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                            alignment: Alignment.centerLeft,
+                            child: FlatButton(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  FlatButton(
+                                      onPressed: null, child: Text("EDIT")),
+                                  FlatButton(
+                                      onPressed: null, child: Text("DELETE"))
+                                ],
+                              ),
+                              onPressed: null,
+                            )),
+                      ),
+                    ],
+                  ),
+                  Divider(height: 2, color: Colors.black12)
+                ],
+              ),
+            )
           ],
         ),
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.only(right: 16, top: 16, left: 16),
+        margin: EdgeInsets.only(bottom: 5),
       ),
       onTap: _onCheckBoxChange,
     );
